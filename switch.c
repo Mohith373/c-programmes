19a4837405458463ee0ee97745eb5ee059c857a2
Mohith373 <mohithias24@gmail.com>
2024-12-20 21:33:33 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int var;
+    printf("which variable you want");
+    scanf("%d",&var);
+    switch (var)
+    {
+    case 1:
+        printf("mohith is very good at ");
+        break;
+    case 2:
+        printf("gopal raj is a very good director:");
+        break;
+    default:
+        printf("venky is so big");
+        break;
+    }
+
+    return 0;
+}
+
