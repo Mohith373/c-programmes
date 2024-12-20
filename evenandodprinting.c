6db9220a2dfa1bddc7def0688931bab1ffd35d45
Mohith373 <mohithias24@gmail.com>
2024-12-20 21:39:01 +0530

    Add files via upload

---
+++ evenandodprinting.c
@@ -0,0 +1,24 @@
+/*#include <stdio.h>
+int main() {
+    for(int i=0;i<=10;i++)
+    {
+        if(i%2==0)
+        continue;
+        printf("the odd numbers is  %d\n",i);
+    }
+
+    return 0;
+}*/
+#include<stdio.h>
+int main()
+{
+    for (int j = 0; j < 10; j++)
+    {
+        if(j%2!=0)
+        continue;
+        printf("even nums are%d\n",j);
+    }
+    
+    return 0;
+}
+
