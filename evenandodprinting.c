d6162b90bde48b8a74b5b8eb82e84553c6803e30
Mohith373 <mohithias24@gmail.com>
2024-12-23 23:10:08 +0530

    Update evenandodprinting.c

--- evenandodprinting.c
+++ evenandodprinting.c
@@ -1,4 +1,4 @@
-/*#include <stdio.h>
+#include <stdio.h>
 int main() {
     for(int i=0;i<=10;i++)
     {
@@ -8,7 +8,7 @@ int main() {
     }
 
     return 0;
-}*/
+}
 #include<stdio.h>
 int main()
 {
