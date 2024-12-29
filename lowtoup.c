b7d61d9c82dc9a60645efe50b89ff5674d90b9ca
Mohith373 <mohithias24@gmail.com>
2024-12-29 10:59:15 +0530

    Update lowtoup.c

--- lowtoup.c
+++ lowtoup.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
-
-void convertCase(char *str) {
-  int i = 0;
-  while (str[i] != '\0') {
-    if (str[i] >= 'A' && str[i] <= 'Z') {
-      str[i] = str[i] + 32; // Convert uppercase to lowercase
-    } else if (str[i] >= 'a' && str[i] <= 'z') {
-      str[i] = str[i] - 32; // Convert lowercase to uppercase
-    }
-    i++;
-  }
-}
+#include <string.h>
 
 int main() {
-  char str[100];
-  printf("Enter a string: ");
-  gets(str); // Note: gets() is unsafe, consider using fgets() in real applications
+   char s[100];
+   int i;
+   printf("\nEnter a string : ");
+   gets(s);
 
-  convertCase(str);
-
-  printf("Converted string: %s\n", str);
-
-  return 0;
+   for (i = 0; s[i]!='\0'; i++) {
+      if(s[i] >= 'a' && s[i] <= 'z') {
+         s[i] = s[i] - 32;
+      }
+   }
+   printf("\nString in Upper Case = %s", s);
+   return 0;
 }
