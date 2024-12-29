b7e49864119290f39e1782e9639f6e4cd486948b
Mohith373 <mohithias24@gmail.com>
2024-12-29 11:18:16 +0530

    Update lowtoup.c

--- lowtoup.c
+++ lowtoup.c
@@ -9,9 +9,12 @@ int main() {
 
    for (i = 0; s[i]!='\0'; i++) {
       if(s[i] >= 'a' && s[i] <= 'z') {
-         s[i] = s[i] - 32;
+         s[i] = s[i] - 32; //small to capital
+      else(s[i]>='A'&&s[i]<=Z)
+         s[i]+=32;//capital to small
       }
    }
    printf("\nString in Upper Case = %s", s);
+   printf("string in lower case=%s",s);
    return 0;
 }
